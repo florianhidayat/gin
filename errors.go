421679766ae5b8771ed60e4e7579ddfc37a17da4
florianhidayat <florian1846@gmail.com>
2020-05-07 19:15:25 +0700

    Fix wrong import in errors.go

--- errors.go
+++ errors.go
@@ -9,7 +9,7 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/florianhidayat/gin/json"
+	"github.com/florianhidayat/gin/internal/json"
 )
 
 // ErrorType is an unsigned 64-bit error code as defined in the gin spec.
